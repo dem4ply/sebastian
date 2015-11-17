fbb436e20fc7191bf08cf0c1e5e547a74eb11982
dem4ply <dem4ply@gmail.com>
2015-11-17 12:15:03 -0600

    [snippet] se agrego documentacion y la prueba unitaria

--- snippet/sp_random.py
+++ snippet/sp_random.py
@@ -4,13 +4,13 @@ funciones para generar randoms
 import random
 import string
 
-def generate_string( l=10 ):
+def generate_string( length=10 ):
 	"""
 	Genera una cadena aleatoria usando las letras de ascii
 
 	Arguments
 	---------
-	l: int
+	length: int
 		longitud del la cadena a generar
 	
 	Returns
@@ -21,5 +21,5 @@ def generate_string( l=10 ):
 	return u''.join(
 		random.choice(
 			string.ascii_letters
-		) for x in range( l )
+		) for x in range( length )
 	)
